fa5063df88c0b669fe6bc4c35c1b3816005c4676
felipetaua <taua.felipee@gmail.com>
2026-02-16 13:46:53 -0300

    ajustando o auth dependendo da plataforma

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence, browserLocalPersistence, getAuth } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Platform } from 'react-native';
 
 const firebaseConfig = {
     apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
@@ -16,8 +17,11 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage),
-});
+
+const auth = Platform.OS === 'web' 
+    ? getAuth(app) 
+    : initializeAuth(app, {
+        persistence: getReactNativePersistence(AsyncStorage),
+    });
 
 export { db, auth, app, firebaseConfig };
